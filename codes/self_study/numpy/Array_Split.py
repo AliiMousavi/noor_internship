dd683eef029dae437321da5a5e3882bc8fe46861
AliiMousavi <AliMousavi04897@gmail.com>
2025-12-12 20:29:26 +0330

    numpy: add array split codes

---
+++ codes/self_study/numpy/Array_Split.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6])
+
+newarr = np.array_split(arr, 3)
+print(newarr)
+
+newarr = np.array_split(arr, 4)
+print(newarr)
+
+newarr = np.array_split(arr, 3)
+print(newarr[0])
+print(newarr[1])
+print(newarr[2])
+
+
+arr = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]])
+newarr = np.array_split(arr, 3)
+print(newarr)
